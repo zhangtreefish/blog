6a8411b09d810995a92085d4adb9e9342ecd20c1
zhangtreefish <zhangtreefish@users.noreply.github.com>
2017-01-08 22:15:27 -0600

    Refactor

--- engineapp/handlers/welcomehandler.py
+++ engineapp/handlers/welcomehandler.py
@@ -5,21 +5,18 @@ import google.appengine.ext.db as db
 
 class WelcomeHandler(bloghandler.BlogHandler):
     def get(self):
+        posts = ''
         # if logged in, show user's own posts
         if self.user:
-            user_posts=models.BlogPost.query_post(self.user.key)
-            self.render(
-                'welcome.html',
-                user_id=self.user.key.id(),
-                username=self.user.username,
-                posts=user_posts
-            )
+            posts=models.BlogPost.query_post(self.user.key)
+        # otherwise show 10 most recent posts by all users
         else:
-            # otherwise show 10 most recent posts by all users
-            message = self.request.get('message')
-            all_posts = models.BlogPost.query().order(-models.BlogPost.postedAt).fetch(10)
-            self.render(
-                'welcome.html',
-                message=message,
-                posts=all_posts
-            )
+            posts = models.BlogPost.query().order(-models.BlogPost.postedAt).fetch(10)
+        message = self.request.get('message')
+        self.render(
+            'welcome.html',
+            message=message,
+            posts=posts,
+            user_id=self.user.key.id() if self.user else None,
+            username=self.user.username if self.user else None
+        )
